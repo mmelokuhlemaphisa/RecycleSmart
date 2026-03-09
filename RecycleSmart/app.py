de43edde9653d7bdc4a03996a1093ad475622064
Melokuhle <melokuhlemaphisa99@gmail.com>
2026-03-09 14:32:45 +0200

    feat: added local host

--- RecycleSmart/app.py
+++ RecycleSmart/app.py
@@ -279,4 +279,4 @@ if __name__ == '__main__':
         print("---------------------------------------")
 
     # Start the server
-    app.run(debug=True, port=8081)
+    app.run(debug=True, host='0.0.0.0', port=8081)
